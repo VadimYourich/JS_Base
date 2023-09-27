a2b630cac2797c042d64a5becfd5359e0c5ac06c
vadim <89281779909@mail.ru>
2023-09-27 19:20:30 +0300

    семинар1

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log("Текст из script.js");
+
+// let a;
+// a = 5;
+// a = 7;
+// console.log(a);
+
+// const a = 1.5;
+// const b = 0.75;
+// const c = a + b;
+// console.log(c);
+
+const a = "Java";
+const b = "Script";
+// console.log(a+b)
+console.log(`${a}${b}`);
