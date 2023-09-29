a69be59f594387eaf5dc5dfd4bf17f87c1378434
vadim <89281779909@mail.ru>
2023-09-29 18:01:14 +0300

    sem2

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+/* // const num = 333;
+// if (num > 15) {
+//   console.log(`${num} больше 15`);
+// } else if (num < 15) {
+//   console.log(`${num} меньше 15`);
+// } else {
+//     console.log(`${num} равно 15`);
+// }
+// const num = 333;
+// switch (true) {
+//     case num > 15:
+//         console.log(`${num} больше 15`);
+//         break;
+//     case num < 15:
+//         console.log(`${num} меньше 15`);
+//         break;
+//     default:
+//         console.log(`${num} равно 15`);
+//         break;
+// }
+const num = 333;
+console.log(
+    (num > 15) ? `${num} больше 15` 
+    : (num > 15) ? `${num} меньше 15` 
+    : `${num} равно 15`
+); */
+
+/* // const youAge = Number(prompt("Какой Ваш возраст?", "Введите Ваш возраст"));
+const youAge = +prompt("Какой Ваш возраст?", "Введите Ваш возраст");
+const youName = prompt("Как Вас зовут?");
+alert(`Вам ${youAge} лет`);
+alert(`Добро пожаловать на сайт ${youName} с возрастом ${youAge} лет`);
+console.log(youAge); */
+
+// parseFloat();
+// parseInt();
+// console.log(Boolean([]));
+
+/* lesson1 */
+
 // console.log("Текст из script.js");
 
 // let a;
@@ -10,7 +50,7 @@
 // const c = a + b;
 // console.log(c);
 
-const a = "Java";
-const b = "Script";
+// const a = "Java";
+// const b = "Script";
 // console.log(a+b)
-console.log(`${a}${b}`);
+// console.log(`${a}${b}`);
