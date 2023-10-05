c6b4ac7b1cbdcbaa28ef2b61b38405a5ba91242c
vadim <89281779909@mail.ru>
2023-10-05 15:33:16 +0300

    sem5

---
+++ lesson5.js
@@ -0,0 +1,54 @@
+/* const daysOfWeek = {
+    1: 'Понедельник',
+    2: 'Вторник',
+    3: 'Среда',
+    4: 'Четверг',
+    5: 'Пятница',
+    6: 'Суббота',
+    7: 'Воскресенье'
+};
+
+console.log(daysOfWeek[2]); */
+
+/* 
+// const username = 'Vlad';
+// const surname = 'Lee';
+// const age = 33;
+
+// function User () {
+//     this.name = username;
+//     this.surname = surname;
+//     this.age = age;
+// }
+
+// const userCard = new User(username, surname, age);
+// console.log(userCard);
+
+const user = {
+  name: "Vlad",
+  surname: "Lee",
+  age: 33,
+};
+
+const { name, surname, age } = user;
+delete user.name;
+console.log(user.name);
+console.log(name);
+
+user.middleName = "Yourich";
+console.log(user);
+
+// console.log(`${user.name} ${user.surname} ${user.age}`);
+ */
+
+/* 
+из массивов собрали объект
+
+const arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+const arr2 = [1,2,3,4,5,6,7];
+const obj = {};
+for (let i = 0; i < arr1.length; i++) {
+    obj[arr1[i]] = arr2[i]
+}
+console.log(obj);
+ */
