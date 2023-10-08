4b7ce254ddeb6347dc5ece50db9762afd9bd0d31
vadim <89281779909@mail.ru>
2023-10-08 16:04:51 +0300

    hw6

---
+++ hw6_ECMA/index.js
@@ -0,0 +1,26 @@
+// 1) Дан массив const arr = [1, 5, 7, 9] с помощью Math.min и spread оператора,
+// найти минимальное число в массиве, решение задание должно состоять из одной
+// строки кода.
+
+const arr = [1, 5, 7, 9];
+console.log(Math.min(...arr));
+
+// 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект
+// с двумя методами: increment и decrement. Метод increment должен увеличивать
+// значение счетчика на 1, а метод decrement должен уменьшать значение счетчика
+// на 1. Значение счетчика должно быть доступно только через методы объекта,
+// а не напрямую.
+
+function createCounter() {
+  let count = 0;
+  return {
+    increment() {
+      return ++count;
+    },
+    decrement() {
+      return --count;
+    },
+  };
+}
+console.log(createCounter().increment());
+console.log(createCounter().decrement());
