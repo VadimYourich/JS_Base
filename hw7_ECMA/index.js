98ea193649658c7e6f060416750e026a687b9bd7
vadim <89281779909@mail.ru>
2023-10-10 10:59:50 +0300

    hw7

--- hw7_ECMA/index.js
+++ hw7_ECMA/index.js
@@ -7,11 +7,22 @@
 // Свойство pages (количество страниц) - число, количество страниц в книге.
 // Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
 
-class Book
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+  displayInfo() {
+    console.log(
+      `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`
+    );
+  }
+}
 
 // // Пример использования класса
-// const book = new Book("BookName1", "Author1", 123);
-// book.displayInfo(); // "Title: BookName1, Author: Author1, Pages: 123"
+const book = new Book("BookName1", "Author1", 123);
+book.displayInfo(); // "Title: BookName1, Author: Author1, Pages: 123"
 
 // Задание 2: "Управление списком студентов"
 // Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
@@ -24,3 +35,16 @@ class Book
 // // Пример использования класса
 // const student = new Student("John Smith", 16, "10th grade");
 // student.displayInfo(); // "Name: John Smith, Age: 16, Grade: 10th grade"
+
+class Student {
+  constructor(name, age, grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+  displayInfo() {
+    console.log(`Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`);
+  }
+}
+const student = new Student("John Smith", 16, "10th grade");
+student.displayInfo();
