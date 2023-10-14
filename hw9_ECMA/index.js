c264f292011324c25b4164f0f850ad3db3a2a832
vadim <89281779909@mail.ru>
2023-10-14 16:09:00 +0300

    hw9

--- hw9_ECMA/index.js
+++ hw9_ECMA/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Задание 1. Получение данных о пользователе.
 
 // Реализуйте функцию getUserData, которая принимает идентификатор пользователя (ID) в качестве аргумента и использует fetch для получения данных о пользователе с заданным ID с удаленного сервера. Функция должна возвращать промис, который разрешается с данными о пользователе в виде объекта. Если пользователь с указанным ID не найден, промис должен быть отклонен с соответствующим сообщением об ошибке.
@@ -6,6 +8,21 @@
 // getUserData использует fetch для получения данных о пользователе с удаленного сервера. Если запрос успешен (с кодом 200), функция извлекает данные из ответа с помощью response.json() и возвращает объект с данными о пользователе. Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
 
 // Работа должна быть выполнена с API: https://reqres.in/
+
+async function getUserData(id) {
+  try {
+    const response = await fetch(`https://reqres.in/api/users/${id}`);
+    if (response.ok) {
+      console.log(await response.json());
+    } else {
+      console.log(`Пользователь с id: ${id} - не найден!`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getUserData(5);
+getUserData(25);
 
 // Задание 2. Отправка данных на сервер.
 
@@ -29,3 +46,26 @@
 // saveUserData использует fetch для отправки данных о пользователе на удаленный сервер для сохранения. Она отправляет POST-запрос на URL-адрес /api/users с указанием типа содержимого application/json и сериализует объект с данными о пользователе в JSON-строку с помощью JSON.stringify(). Если запрос успешен (с кодом 201), функция разрешает промис. Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
 
 // Работа должна быть выполнена с API: https://reqres.in/
+
+const user = {
+  name: "John Doe",
+  job: "unknown",
+};
+
+async function saveUserData(user) {
+  let response = await fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(user),
+  })
+    .then(() => {
+      console.log("User data saved successfully");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+
+saveUserData(user);
