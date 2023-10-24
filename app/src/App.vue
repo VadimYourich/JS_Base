8a2afe7d20053bec592ab8954323c09025eb225d
vadim <89281779909@mail.ru>
2023-10-24 23:14:16 +0300

    vue4

---
+++ app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Привет из App.vue</h1>
+  <HelloComponent />
+  <WorkGalary />
+</template>
+
+<script>
+import HelloComponent from "./components/HelloComponent.vue";
+import WorkGalary from "./components/WorkGalary.vue";
+
+export default {
+  components: { HelloComponent, WorkGalary },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ app/src/components/WorkGalary.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <img class="big" :src="bigImg" width="500" />
+    <div>
+      <img
+        v-for="(cat, index) in cats"
+        :key="index"
+        :src="cat"
+        @click="chengeImg(cat)"
+        width="100"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import img1 from "@/assets/img/OIP.jpg";
+import img2 from "@/assets/img/OIP (1).jpg";
+import img3 from "@/assets/img/OIP (2).jpg";
+export default {
+  data() {
+    return {
+      cats: {
+        cat1: require("@/assets/img/krasivye-kartinki-kotov-1.jpg"),
+        cat2: img1,
+        cat3: img2,
+        cat4: img3,
+      },
+      bigImg: "",
+    };
+  },
+  methods: {
+    chengeImg(catUrl) {
+      this.bigImg = catUrl;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
