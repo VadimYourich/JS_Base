2d809d3c34d828685e5eb44de08975e37e5c4265
vadim <89281779909@mail.ru>
2023-10-26 22:49:27 +0300

    vue5

---
+++ vue5/app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<AuthComponent :isAuth="false" />
+	<hr />
+	<UserListComponent />
+	<hr />
+	<MortageCalculatorComponent />
+</template>
+
+<script>
+import AuthComponent from './components/AuthComponent.vue';
+import MortageCalculatorComponent from './components/MortageCalculatorComponent.vue';
+import UserListComponent from './components/UserListComponent.vue';
+export default {
+	components: {
+		AuthComponent,
+		UserListComponent,
+		MortageCalculatorComponent,
+	},
+};
+</script>
+
+<style lang="scss"></style>
