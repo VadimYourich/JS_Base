f9b7403e2f18535fdc910ed9c26490cb4ee22fb3
vadim <89281779909@mail.ru>
2023-11-22 15:05:07 +0300

    hw_1 API

---
+++ JS про API браузеров/HW_1/dist/task1.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+/*  Отслеживание изменения ориентации экрана:
+Напишите код, который отслеживает изменение ориентации экрана устройства (с портретной на ландшафтную и наоборот) и выводит сообщение об этом на веб-странице. */
+
+function trackingScreenOrientation() {
+  if (window.matchMedia("(orientation: portrait)").matches) {
+    alert("Ориентации экрана изменена на альбомную");
+  } else {
+    alert("Ориентации экрана изменена на портретную");
+  }
+}
+
+window.addEventListener("resize", function () {
+  window.addEventListener("orientationchange", trackingScreenOrientation);
+});
