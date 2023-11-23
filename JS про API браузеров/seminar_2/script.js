b273ae8389babe079b06c2d4d3d1a443f1bfe5be
vadim <89281779909@mail.ru>
2023-11-23 22:39:11 +0300

    sem_2 API

---
+++ JS про API браузеров/seminar_2/script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+/* Вам необходимо создать навигационное меню для веб-сайта, в котором меняется активный пункт при клике без фактического перехода на другие страницы. Меню должно обладать следующими характеристиками:
+Подсветка активного пункта: При клике на пункт меню он должен становиться активным, и для активного пункта должен применяться определенный стиль (например, изменение цвета фона). Если выбрать другой пункт, предыдущий должен перестать быть активным.
+Эффекты наведения: При наведении курсора на пункты меню должны применяться эффекты (например, изменение цвета текста или фона) для подсказки пользователю, что пункт меню является интерактивным. */
+
+// document.querySelector("ul").addEventListener("mouseover", function (e) {
+//   if (e.target.tagName === "A") {
+//     e.target.style.backgroundColor = "#f0f0f0";
+//   }
+// });
+// document.querySelector("ul").addEventListener("mouseout", function (e) {
+//   if (e.target.tagName === "A") {
+//     e.target.style.backgroundColor = "#ffffff";
+//   }
+// });
+// document.querySelector("ul").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   const activeEl = document.querySelector(".active");
+//   if (activeEl) {
+//     activeEl.classList.remove("active");
+//   }
+//   if (e.target.tagName === "A") {
+//     e.target.classList.add("active");
+//   }
+// });
+
+/* Task_2   Модальное окно */
+
+// const modalWin = document.querySelector(".modalWin");
+// const btn = document.querySelector(".btn");
+// btn.addEventListener("click", () => {
+//   modalWin.classList.add("is-open");
+// });
+
+// modalWin.addEventListener("click", () => {
+//   modalWin.classList.remove("is-open");
+// });
+
+/* Task_3 */
+
+const button = document.querySelector(".button");
+if (button) {
+  document.querySelector(".button").addEventListener("click", (e) => {
+    if (e.isTrusted) {
+      const text = e.target.textContent;
+      e.target.textContent = "Товар добавлен в корзину";
+      setTimeout(() => {
+        e.target.textContent = text;
+      }, 2000);
+    }
+  });
+}
+
+window.addEventListener("load", function (e) {
+  if (getLastPartOfUrl(window.location.href) === "product.html") {
+    setTimeout(() => {
+      history.back();
+    }, 2000);
+  }
+});
+
+function getLastPartOfUrl(url) {
+  const parts = url.split("/");
+  return parts.at(-1);
+}
